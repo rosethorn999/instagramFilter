df944c5e6c6fc0e4454dd8c60f67f445db1789f0
rosethorn999 <allen972523+github@gmail.com>
2017-04-24 00:08:53 +0900

    img filter css

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
     'Contrast': 100,
     'Brightness': 100,
     'Saturate': 100,
-    'Sepia': 100,
+    'Sepia': 0,
     'Grayscale': 0,
     'Invert': 0,
     'Hue Rotate': 0,
@@ -26,6 +26,20 @@ export class DataService {
   // Service message commands
   insertData(data: string) {
     this.dataStringSource.next(data);
+  }
+
+  figureChange(){
+    let cssStr :string= "";
+    cssStr += "contrast(" + this.silderData['Contrast'] + "%) " ;
+    cssStr += "brightness(" + this.silderData['Brightness'] + "%) " ;
+    cssStr += "saturate(" + this.silderData['Saturate'] + "%) " ;
+    cssStr += "sepia(" + this.silderData['Sepia'] + "%) " ;
+    cssStr += "grayscale(" + this.silderData['Grayscale'] + "%) " ;
+    cssStr += "invert(" + this.silderData['Invert'] + "%) " ;
+    cssStr += "hue-rotate(" + this.silderData['Hue Rotate'] + "deg) " ;
+    cssStr += "blur(" + this.silderData['Blur'] + "px)" ;
+    let image :any = document.querySelector('.fi');
+    image.style.filter = cssStr;
   }
 
 }
