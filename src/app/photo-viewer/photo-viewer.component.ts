31024ff065b8bc1b3c58d281a7dbccd9df4af706
rosethorn999 <allen972523+github@gmail.com>
2017-04-23 23:49:26 +0800

    刪除photo-viewer.component中不必要的事件

--- src/app/photo-viewer/photo-viewer.component.ts
+++ src/app/photo-viewer/photo-viewer.component.ts
@@ -19,17 +19,5 @@ export class PhotoViewerComponent implements OnInit {
       data => {
         this.filterSelected = data;
       });
-      this.figureChange();
   }
-
-  figureChange(){
-    let image = document.querySelector('.fi');
-    debugger;
-    image[1].style.filter = "";
-    //image.style.backgroundColor = "green";
-  }
-
-
-
-
 }
